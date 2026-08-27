bfd3ab226adcc3b3f1281fac357291954554b170
Leonid Medovyy <leonid@macron1.io>
2026-08-27 16:18:44 -0700

    Make sidebar panels scroll independently of the chat pane

--- src/components/sessions-sidebar.tsx
+++ src/components/sessions-sidebar.tsx
@@ -75,7 +75,7 @@ export function SessionsSidebar() {
   }, []);
 
   return (
-    <aside className="hidden w-64 shrink-0 flex-col border-r border-zinc-200 dark:border-zinc-800 lg:flex">
+    <aside className="hidden min-h-0 w-64 shrink-0 flex-col border-r border-zinc-200 dark:border-zinc-800 lg:flex">
       <div className="flex items-center justify-between px-4 pt-5 pb-3">
         <Link
           href="/"
